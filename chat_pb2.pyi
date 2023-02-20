c79e31688c147e78205ff8421d70471492e31601
James Barna <jamesbarna@college.harvard.edu>
2023-02-20 16:18:06 -0500

    gRPC send done before testing 2

---
+++ chat_pb2.pyi
@@ -0,0 +1,35 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Payload(_message.Message):
+    __slots__ = ["msg"]
+    MSG_FIELD_NUMBER: _ClassVar[int]
+    msg: str
+    def __init__(self, msg: _Optional[str] = ...) -> None: ...
+
+class SendRequest(_message.Message):
+    __slots__ = ["recipient", "sender", "sentMsg"]
+    RECIPIENT_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    SENTMSG_FIELD_NUMBER: _ClassVar[int]
+    recipient: Username
+    sender: Username
+    sentMsg: Payload
+    def __init__(self, sender: _Optional[_Union[Username, _Mapping]] = ..., recipient: _Optional[_Union[Username, _Mapping]] = ..., sentMsg: _Optional[_Union[Payload, _Mapping]] = ...) -> None: ...
+
+class Unreads(_message.Message):
+    __slots__ = ["errorFlag", "unreads"]
+    ERRORFLAG_FIELD_NUMBER: _ClassVar[int]
+    UNREADS_FIELD_NUMBER: _ClassVar[int]
+    errorFlag: bool
+    unreads: str
+    def __init__(self, errorFlag: bool = ..., unreads: _Optional[str] = ...) -> None: ...
+
+class Username(_message.Message):
+    __slots__ = ["name"]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    def __init__(self, name: _Optional[str] = ...) -> None: ...
